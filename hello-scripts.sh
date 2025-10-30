4328730c126504e1fe2eef134ca30961c3f7120c
punit1407 <ubuntu@ip-172-31-42-195.ap-south-1.compute.internal>
2025-10-30 12:06:43 +0000

    My first script

---
+++ hello-scripts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello world"
+echo "Hello OM"
+echo "Hello Manish"
+echo "govind"
+
+echo "om please make a directory"
+
+mkdir -p Devops
+
+echo "My name is manish" > manish.txt
---
+++ variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+Hero="Manish"
+
+Villan="Om"
+
+echo "Batch ka hero kon $Hero"
+echo "Batch ka villan kon $Villan"
